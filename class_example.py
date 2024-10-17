8d7684e279d2ffa41dafb6f958abe05a8e20230a
trevornei <117675657+trevornei@users.noreply.github.com>
2024-10-17 12:48:32 -0600

    Added new file and updating class_examples.py

--- class_example.py
+++ class_example.py
@@ -1,13 +1,28 @@
 
 
 class Vehicle:
-    def __init__(self, make, model, feul="gas"):
+    def __init__(self, make, model, fuel="gas", num_wheels=4):
         self.make = make
         self.model = model
-        self.fuel = feul
+        self.fuel = fuel 
+        self.num_wheels = num_wheels
+
+    def __str__(self):
+        # str's methods need a return statement.
+        return f"My daily runner is a {self.make} {self.model}. It runs on {self.fuel}"
+
+class Car(Vehicle):
+
+    number_of_wheels = 4
+
+class Truck(Vehicle):
+
+    number_ofwheels = 8
+    def __init__(self, make, model, fuel="Diesel",):
+        super().__init__(self, make, model, fuel) 
+
 
 daily = Vehicle("Toyota", "4Runner")
-# print(daily_vehicle)
+print(daily)
 
-# print with f string.
-print(f"I drive a {daily.make} {daily.model} as a daily driver.")
+print("for Class", daily.num_wheels)
